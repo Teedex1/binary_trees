7dae53a5691b737546badde01d278e4331ed9814
Tunde <tundeogunmodede1@gmail.com>
2024-03-26 04:11:03 -0700

    new fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,13 +6,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_height_1 = 0;
+	size_t height_i = 0;
 	size_t height_r = 0;
 
 	if (!tree)
 		return (0);
 
-	height_1 = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_i = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (height_1 > height_r ? heoght_1 : height_r);
+	return (height_i > height_r ? height_i : height_r);
 }
